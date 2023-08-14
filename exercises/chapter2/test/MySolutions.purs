72412fe38d6fcb23ac082384e706b8bff4140b00
SealBelek <o.azzmo@gmail.com>
2023-08-14 22:47:58 +0300

    chapter 2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -2,8 +2,14 @@ module Test.MySolutions where
 
 import Prelude
 
+import Data.Int (rem)
 import Data.Number (pi, sqrt)
 
+diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
 
+circleArea :: Number -> Number
 circleArea r = pi * r * r
+
+leftoverCents :: Int -> Int
+leftoverCents c = rem c 100
